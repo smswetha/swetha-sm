79e47ca2f49f5de3d1873abc8fce1e530c90f1db
smswetha <swethasm17022001@gmail.com>
2022-08-10 09:26:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# swetha-sm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/swetha-sm)
---
+++ src/app/dir.config.ts
@@ -0,0 +1,15 @@
+import { MenuDir } from './directives/menu.dir';
+import { ProductsListDir } from './directives/productlist.dir';
+import { AddToCartDir } from './directives/addtocart.dir';
+import { BillingDir } from './directives/billing.dir';
+import { BillingCartDir } from './directives/billingcart.dir';
+import { CheckOutDir } from './directives/checkout.dir';
+
+export const dirConfig = [
+  MenuDir,
+  ProductsListDir,
+  AddToCartDir,
+  BillingDir,
+  BillingCartDir,
+  CheckOutDir,
+];
